6e74dd24a27e9d1a9f7629b1414bfa14c7496b41
Igor Drozdov <igor.drozdov@iii.com>
2018-10-07 22:10:05 +0300

    fix connnection and complete docker compose

--- loader/src/main/java/ico/hh/service/SegmentClient.java
+++ loader/src/main/java/ico/hh/service/SegmentClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "segment", url = "${discovery.url}")
+@FeignClient(value = "segment", url = "http://" + "${discovery.host}" + ":${discovery.port}")
 public interface SegmentClient {
 
   @RequestMapping(method = RequestMethod.POST, value = "/segment")
